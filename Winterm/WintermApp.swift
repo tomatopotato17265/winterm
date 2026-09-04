1ce25cfa373cd41a7aa0aa502a914e94673ce3dd
tomato potato <nimaigos108@gmail.com>
2026-09-04 05:46:19 -0700

    intial commit

---
+++ Winterm/WintermApp.swift
@@ -0,0 +1,15 @@
+//
+//  WintermApp.swift
+//  Winterm
+//
+
+import SwiftUI
+
+@main
+struct WintermApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
